0f08f3293c5c2718a280f61e8a660f81e71b6bde
ptimperman <ptimperman@gmail.com>
2018-02-21 13:54:12 -0500

    Running model for single loop from ten images 80% accuracy

--- defect_detection.py
+++ defect_detection.py
@@ -14,6 +14,7 @@
 
 #Import statements 
 import numpy 
+import sys 
 import os 
 from scipy import misc #For reading in images 
 import pdb
@@ -21,6 +22,10 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 import keras 
+
+
+#error= open("error.txt", "w")
+#sys.stderr = error
 #Training images preproccessing 
 data_path = '/home/ptimperman/defect_workspace/images/'
 loop_files = dict() #Dictionary that maps image name to corordinates of defects  
@@ -43,24 +48,30 @@ for raw_image in os.listdir(data_path+'aug_training_positive'):
         else:
                 print ("Could not find data file corresponding to: ", image_name)
 #Convert into list of
-x_train =  list()
-y_train = list()
+x_set =  list()
+y_set = list()
 for data_pair in train_data:
-        y_train.append(data_pair[1])
-        x_train.append(data_pair[0])
+        y_set.append(data_pair[1])
+        x_set.append(data_pair[0])
         #print(data_pair[0].shape)
         #print(data_pair[1].shape)
 
 #Set up model
-x_train = numpy.array(x_train[0:10]).reshape((10,1024,1024,1)) 
-
+x_train = numpy.array(x_set[0:10]).reshape((10,1024,1024,1)) 
+x_test = numpy.array(x_set[11:21]).reshape((10,1024,1024,1))
 #y_train = numpy.array(y_train[0:10]).reshape((10,4,1,1)) 
-y_train= numpy.array(y_train[0:10])
+y_train= numpy.array(y_set[0:10])
+y_test = numpy.array(y_set[11:21])
+
+number_of_kernels = 40
+nuerons_in_layer = 40 
+epochs = 20 
+
 model = Sequential()
-model.add(Conv2D(32, kernel_size=(265,265), strides=(1,1), activation ='relu', data_format='channels_last',  input_shape=(1024,1024,1)))
+model.add(Conv2D(number_of_kernels, kernel_size=(5,5), strides=(1,1), activation ='relu', data_format='channels_last',  input_shape=(1024,1024,1)))
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2)))
 model.add(Flatten())
-model.add(Dense(1000, activation='relu'))
+model.add(Dense(nuerons_in_layer, activation='relu'))
 model.add(Dense(4, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.SGD(lr=.01), metrics=['accuracy'])
@@ -68,12 +79,16 @@ print("Model compiled.")
 #pdb.set_trace()
 print(x_train.shape)
 print(y_train.shape)
-model.fit(x_train ,y_train,batch_size=2, epochs=10, verbose = 1, validation_data=(numpy.array(x_train[11:21]), numpy.array(y_train[11:21])))
-score = model.evaluate(x_train[11:21], y_train[11:21], verbose=1)
-print(score[0])
-print(score[1])
-
-        
+print(x_test.shape)
+print(y_test.shape)
+model.fit(x_train ,y_train,batch_size=2, epochs= epochs ,verbose = 1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=1)
+print(score)
+with open('single_loop_detection log.txt', 'a') as log_file:
+        log_file.write("Number of kernels: " + str(number_of_kernels)+"\n")
+        log_file.write("Nuerons in each layer: "+ str(nuerons_in_layer)+"\n")
+        log_file.write("Epochs: " + str(epochs)+"\n")
+        log_file.write("Score: " + str(score[1])+"\n") 
 
 
 
